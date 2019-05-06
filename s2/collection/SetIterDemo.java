80161766bb666a1f1c36bb872ce5d0b9bffa74de
Dr. Stefan Schneider <stsch@amazon.de>
2019-05-06 11:08:15 +0200

    Fix SetIterDemo Aufgabe

--- s2/collection/SetIterDemo.java
+++ s2/collection/SetIterDemo.java
@@ -7,29 +7,19 @@ import java.util.TreeSet;
 public class SetIterDemo {
 
 public static void main(String[] args) {
-        Set<Student> mengeUnsortiert = new HashSet<Student>(); // Hier HashSetobjekt einfügen
-        Set<Student> mengeSortiert =   new TreeSet<Student>(); // Hier TreeSetobjekt einfügen
+        Set<Student> mengeUnsortiert = null; // Hier HashSetobjekt einfügen
+        Set<Student> mengeSortiert = null; // Hier TreeSetobjekt einfügen
         Student s;
         s = new Student("Curie", "Marie", 19, 1);
         System.out.println("Hier Student " + s + "in beide Mengen einfügen");
-        mengeUnsortiert.add(s);
-        mengeSortiert.add(s);
         s = new Student("Merian", "Maria-Sybilla", 17, 3);
         System.out.println("Hier Student " + s + "in beide Mengen einfügen");
-        mengeUnsortiert.add(s);
-        mengeSortiert.add(s);
         s = new Student("Noether", "Emmi", 16, 1);
         System.out.println("Hier Student " + s + "in beide Mengen einfügen");
-        mengeUnsortiert.add(s);
-        mengeSortiert.add(s);
         s = new Student("Liese", "Meitner", 15, 2);
         System.out.println("Hier Student " + s + "in beide Mengen einfügen");
-        mengeUnsortiert.add(s);
-        mengeSortiert.add(s);
         s = new Student("Herschel", "Caroline", 15, 2);
         System.out.println("Hier Student " + s + "in beide Mengen einfügen");
-        mengeUnsortiert.add(s);
-        mengeSortiert.add(s);
         ausgaben(mengeUnsortiert);
         ausgaben(mengeSortiert);
     }
@@ -37,12 +27,11 @@ public static void main(String[] args) {
 public static void ausgaben(Set<Student> menge) {
         Student s;
         System.out.println("Hier Iterator deklarieren");
-        Iterator<Student> iter = menge.iterator();
         System.out.println("Inhalt der Menge ("
                 + menge.getClass() + "):"); // Nicht ersetzen
-        while (iter.hasNext()) { // Abbruchbedingung ersetzen. Die Schleife terminiert so nicht!
+        while (true) { // Abbruchbedingung ersetzen. Die Schleife terminiert so nicht!
             System.out.println("Hier Student aus Iterator in Schleife auslesen");
-            s = iter.next();
+            s = null;
             System.out.println("Student: " + s); // Nicht ersetzen
         }
     }
