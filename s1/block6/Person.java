f144cf37baca65a6fab976bc8de5ef4c87965359
Dr. Stefan Schneider <stsch@amazon.de>
2019-11-14 18:16:20 +0100

    Implementierung tiefer Vergleich für Person

--- s1/block6/Person.java
+++ s1/block6/Person.java
@@ -31,18 +31,59 @@ public class Person {
 
     }
 
+    public boolean equals(Object obj) {
+        Person target;
+        // Type checking is optional for this level of skills
+        if (this.getClass() == obj.getClass() ) {
+            target = (Person) obj;
+            boolean isEqual = false;
+            isEqual = (((name ==null) && (target.name == null))
+                       || ((name != null) && (name.equals(target.name))
+                       ));
+            isEqual = (isEqual &&
+                    (((mutter== null) && (target.mutter == null)) ||
+                            ((mutter!= null) &&
+                             (target.mutter!= null) &&
+                             mutter.equals(target.mutter))
+                    ));
+            isEqual = ( isEqual &&
+                    (((vater== null) && (target.vater == null)) ||
+                            ((vater!= null) &&
+                             (target.vater!= null) &&
+                                    vater.equals(target.vater))
+                    ));
+            return isEqual;
+        }
+        else return false;
+    }
+
     public static void main(String[] args) {
         Person p;
-        Person sonstwie;
+        Person pp;
         for(int i=0; i<=100; i++) {
             System.out.println("Schleifendurchlauf: " + i);
             p = new Person(5);
             p.name = "ich";
-            sonstwie = new Person(p);
+            pp = new Person(p);
 
             System.out.println("z: " + zaehler + " p: " + p + " " +
                     p.name + " mutter: " + p.mutter);
 
+            Person p1 = new Person(3);
+                p1.name = "Willie";
+            Person p2 = new Person(p1);
+
+            System.out.println ( p1.name + " und " + p2.name + " sind " +
+                    (p1.equals(p2)?"gleich":"ungleich") );
+
+            p2.mutter.mutter = null; // Oma ist gestorben :-(
+            System.out.println ( p1.name + " und " + p2.name + " sind " +
+                    (p1.equals(p2)?"gleich":"ungleich") );
+
+            p2 = new Person(p1);
+            p2.mutter.vater = null; // Opa ist gestorben :-(
+            System.out.println ( p1.name + " und " + p2.name + " sind " +
+                    (p1.equals(p2)?"gleich":"ungleich") );
         }
     }
 }
