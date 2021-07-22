45af7f2c07476ee9b6ec4fe0b26b9afd5f08ca13
Dr. Stefan Schneider <s@scalingbits.com>
2021-07-22 16:03:20 +0200

    Kommentare hinzugefuegt

--- s1/airlineSolution/block1/Passagierflugzeug.java
+++ s1/airlineSolution/block1/Passagierflugzeug.java
@@ -5,8 +5,14 @@ package s1.airlineSolution.block1;
  * @author s@scalingbits.com
  */
 public class Passagierflugzeug {
+    /**
+     * Das ist ein Attribut. Eine Zeichenkette dient zu bestimmen des Flugzeugs
+     */
     String kennzeichen;
 
+    /**
+     * Die Methode drucken, druckt alle Information zum Flugzeug auf die Konsole
+     */
     public void drucken() {
         System.out.println("<--------------------->");
         System.out.println("Kennzeichen: " +kennzeichen);
